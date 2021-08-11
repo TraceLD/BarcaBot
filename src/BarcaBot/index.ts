a01105a82de469cba37ad0093d070efcc7832496
TraceLD <lukaszdabrowski881@gmail.com>
2021-08-12 01:36:28 +0200

    add bot boilerplate

--- src/BarcaBot/index.ts
+++ src/BarcaBot/index.ts
@@ -1,1 +1,12 @@
-console.log("Hellos");
+import { Client, Collection, Intents } from "discord.js";
+import { token } from "./config.json";
+
+const client: Client = new Client({
+  intents: [Intents.FLAGS.GUILDS],
+});
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.login(token);
