5aa42cf2c1f6a4a334c5fdeb8daf960f19dece0c
TraceLD <lukaszdabrowski881@gmail.com>
2021-08-17 01:23:37 +0200

    clean up logger

--- src/BarcaBot/logger.ts
+++ src/BarcaBot/logger.ts
@@ -1,10 +1,11 @@
-import winston from "winston";
+import winston, { Logform } from "winston";
 
+const format: Logform.Format = winston.format.combine(
+  winston.format.json(),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+);
 const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.json(),
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-  ),
+  format: format,
   transports: [
     new winston.transports.Console({
       level: "info",
@@ -15,10 +16,7 @@ const logger = winston.createLogger({
       dirname: "logs",
       filename: "errors.log",
       level: "error",
-      format: winston.format.combine(
-        winston.format.json(),
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-      ),
+      format: format,
       handleExceptions: true,
       maxsize: 52428800,
       maxFiles: 3,
@@ -27,10 +25,7 @@ const logger = winston.createLogger({
       dirname: "logs",
       filename: "combined.log",
       level: "info",
-      format: winston.format.combine(
-        winston.format.json(),
-        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-      ),
+      format: format,
       handleExceptions: true,
       maxsize: 5242880,
       maxFiles: 3,
