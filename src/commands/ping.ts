be53f24cf209b7798610d75cfdee9edf63dd2812
TraceLD <lukaszdabrowski881@gmail.com>
2021-08-26 05:09:23 +0200

    code cleanup

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,6 @@
 import { CommandInteraction } from "discord.js";
 import { SlashCommandBuilder, SlashCommandStringOption } from "@discordjs/builders";
 import { ISlashCommand } from "../slashCommand";
-import players from "../api/endpoints/players";
-import logger from "../logger";
 
 const command: ISlashCommand = {
   data: new SlashCommandBuilder()
@@ -13,9 +11,7 @@ const command: ISlashCommand = {
     .setDescription("Replies with pong"),
 
   execute: async (interaction: CommandInteraction) => {
-    const ps = await players.get();
-    logger.debug(JSON.stringify(ps));
-    await interaction.reply("Hi");
+    await interaction.reply("Pong!");
   },
 };
 
