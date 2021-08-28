0096427b950c2d4a89fea2b7d9dcb05fb5639b55
TraceLD <lukaszdabrowski881@gmail.com>
2021-08-28 02:15:54 +0200

    add /about and /invite

--- src/commands/about.ts
+++ src/commands/about.ts
@@ -1,24 +1,36 @@
 import { CommandInteraction, MessageActionRow, MessageButton } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { ISlashCommand } from "../slashCommand";
+import { discordConfig } from "../config.json";
 import { inviteLink } from "../helpers/invite-link";
 import buttonEmojis from "../helpers/button-emojis";
 
 const command: ISlashCommand = {
   data: new SlashCommandBuilder()
-    .setName("invite")
-    .setDescription("Shows an invite link for BarcaBot"),
+    .setName("about")
+    .setDescription("Shows information about BarcaBot"),
 
   execute: async (interaction: CommandInteraction) => {
+    const text = `<@${discordConfig.clientId}> is a FC Barcelona themed Discord bot created by <@303570168372789250>.`;
     const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setLabel("Website")
+        .setStyle("LINK")
+        .setURL("https://traceld.github.io/BarcaBot/")
+        .setEmoji(buttonEmojis.barcaLogo),
       new MessageButton()
         .setLabel("Invite BarcaBot to your server")
         .setStyle("LINK")
         .setURL(inviteLink)
         .setEmoji(buttonEmojis.discordLogo),
+      new MessageButton()
+        .setLabel("GitHub")
+        .setStyle("LINK")
+        .setURL("https://github.com/TraceLD/BarcaBot")
+        .setEmoji(buttonEmojis.githubLogo),
     );
 
-    await interaction.reply({ content: ".", components: [row] });
+    await interaction.reply({ content: text, components: [row] });
   },
 };
 
---
+++ src/helpers/button-emojis.ts
@@ -0,0 +1,5 @@
+export default {
+  barcaLogo: "880958214382968832",
+  discordLogo: "880957470099537960",
+  githubLogo: "880958174398677013",
+};
